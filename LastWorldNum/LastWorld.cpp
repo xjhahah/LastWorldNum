db46f28c12bb07b598ea9a53761a9662564cb39a
xjhahah <908234867@qq.com>
2018-11-14 23:11:50 +0800

    修改代码

--- LastWorldNum/LastWorld.cpp
+++ LastWorldNum/LastWorld.cpp
@@ -5,16 +5,17 @@ using namespace std;
 class Lastworld
 {
 public:
-	size_t LastWorldNum(string s)
+	size_t LastWorldNum()
 	{
+		string s;
 		size_t len;
+		//这里用 getline()函数是因为如果我们输入的是一个语句的话中间势必会有空格，如果用普通的cin输入的话，遇到空格就会停止读取数据，最终得到的结果会出人意料
 		while (getline(cin, s))
 		{
 			size_t pos = s.rfind(' ');
-			len=s.size() - pos - 1;
-			cout << len << " " <<endl;
-		}
-		cout << len << endl;
+			len = s.size() - pos - 1;
+			cout << len << " " << endl;
+		}	
 		return len;
 	}
 private:
@@ -23,6 +24,6 @@ private:
 int main()
 {
 	Lastworld s;
-	s.LastWorldNum(" ");
+	s.LastWorldNum();
 	return 0;
 }
